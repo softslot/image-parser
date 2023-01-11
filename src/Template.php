f6de79c94972044d31d5824923648a448be3f664
Konstantin Korolenko <softslot-com@yandex.ru>
2023-01-12 02:38:47 +0300

    add buildFullPathToTemplate func

--- src/Template.php
+++ src/Template.php
@@ -6,7 +6,25 @@ function render(string $template, array $variables): string
 {
     extract($variables);
     ob_start();
-    include __DIR__ . "/resources/views/{$template}.phtml";
 
-    return ob_get_clean();
+    $templatepath = buildFullPathToTemplate($template);
+    include $templatepath;
+
+    $html = ob_get_clean();
+
+    return $html;
+}
+
+function buildFullPathToTemplate($template)
+{
+    $parts = [
+        getcwd(),
+        'resources',
+        'views',
+        "{$template}.phtml"
+    ];
+
+    $path = implode(DIRECTORY_SEPARATOR, $parts);
+
+    return $path;
 }
